8a62b49e4aca067ae4e1959af3304ee6233736c5
Bruce0921 <114181725+Bruce0921@users.noreply.github.com>
2023-08-19 16:19:09 -0700

    mnist HandWritten_Digit_Classification
    Trying DeepLearning Project,
    currently got 97.7% of accuracy in final testing
    still have space to improve

---
+++ HandWrittenRecognition.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+print(tf.__version__)
+
---
+++ mnist_loader.py
@@ -0,0 +1,14 @@
+
+# from tensorflow import keras
+# from tensorflow.python import keras
+
+# plt.style.use('seaborn-whitegrid')
+# # Set Matplotlib defaults
+# plt.rc('figure', autolayout=True)
+# plt.rc('axes', labelweight='bold', labelsize='large',
+#        titleweight='bold', titlesize=18, titlepad=10)
+
+
+# from tensorflow.keras.datasets import mnist
+
+# (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
